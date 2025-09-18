fd6f8ca8102ca9ff91b28e1014514a2ce93b6589
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-09-18 14:09:10 +0200

    feat: implement Toggl time entries API fetching and local JSON storage

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+import requests
+import json
+from base64 import b64encode
+from dotenv import load_dotenv
+
+load_dotenv()
+API_TOKEN = os.getenv("API_TOKEN")
+
 def main():
-    print("Hello from toggle-time-entries-extractor!")
+    data = requests.get('https://api.track.toggl.com/api/v9/me/time_entries', headers={'content-type': 'application/json', 'Authorization' : 'Basic %s' %  b64encode(f"{API_TOKEN}:api_token".encode("ascii")).decode("ascii")})
+    
+    os.makedirs("data", exist_ok=True)
+    with open("data/time_entries.json", "w") as f:
+        for time_entry in data.json():
+            f.write(json.dumps(time_entry) + "\n")
+
+    print("Time entries extracted successfully!")
 
 
 if __name__ == "__main__":
